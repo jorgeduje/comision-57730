674c29c52ee5d44280eca0cf3ed4b4a9d2e2adfc
jorgeduje <jorgeduje4@gmail.com>
2024-06-10 22:34:51 -0300

    clase-1

---
+++ variables.js
@@ -0,0 +1,49 @@
+// VARIABLES y TIPOS DE DATOS PRIMITIVOS
+
+let nombre = "pepe";
+let apellido = "perez"; // ---> string
+
+// alert(nombre);
+
+console.log(nombre);
+// console.log(apellido);
+
+const documento = 123123123; // ---> number
+
+nombre = "juancito";
+
+console.log(nombre);
+
+// documento = "11111111"; // Esto no se puede
+// let edad_de_la_persona = 31;
+let edadDeLaPersona = 31;
+console.log(edadDeLaPersona);
+
+// let numero = prompt("por favor ingresa un numero");
+
+// console.log(numero);
+
+let precio = 15.5;
+
+// let nombreDeUsuario = prompt("por favor inicia sesion con tu nombre");
+
+// alert("hola " + nombreDeUsuario + " como estas?");
+// let n1 = prompt("ingresa el primer numero");
+// let n2 = prompt("ingresa el segundo numero");
+
+let n1 = Number(prompt("ingresa el primer numero"));
+
+let n2 = Number(prompt("ingresa el segundo numero"));
+
+let total = n1 + n2;
+
+console.log(total);
+
+// let x = "12";
+// console.log(x);
+// Number(x);
+// console.log(x);
+
+// boolean ---> booleanos
+
+let elUsuarioEstaLogeado = true;
